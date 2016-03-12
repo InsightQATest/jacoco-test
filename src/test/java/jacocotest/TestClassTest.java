7e9ed47bb660509bd9567c20f3f7a0a980bf835d
Radovan Kovalcik <radovankovalcik@gmail.com>
2016-03-12 13:03:05 +0100

    add failing test

--- src/test/java/jacocotest/TestClassTest.java
+++ src/test/java/jacocotest/TestClassTest.java
@@ -3,11 +3,18 @@ package jacocotest;
 import com.kovalcik.jacocotest.TestClass;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class TestClassTest {
 
     @Test
     public void testHello() {
         TestClass testClass = new TestClass();
         testClass.doSomething("ok");
+    }
+
+    @Test
+    public void testFail() {
+        fail();
     }
 }
